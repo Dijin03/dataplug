9663106bfe5e647ac3d935093c0e09fef1f074d9
Dijin03 <bscs17003@itu.edu.pk>
2021-01-21 10:25:12 -0600

    Update users.php
    /controllers/users.php Line #875- Line is greater then 80 characters

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -885,7 +885,8 @@ class Users extends CI_Controller {
             }
             $this->form_validation->set_rules('group_id',
              'Group', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('first_name', 'First Name', 'trim|required|min_length[1]|xss_clean');
+            $this->form_validation->set_rules('first_name', 
+                'First Name', 'trim|required|min_length[1]|xss_clean');
             $this->form_validation->set_rules('last_name', 'Last Name', 'trim|required|min_length[1]|xss_clean');
             //$this->form_validation->set_rules('username', 'User Name', 'trim|required|callback_username_not_available');
             $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|callback_email_not_available');
